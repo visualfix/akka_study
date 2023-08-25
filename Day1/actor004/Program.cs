805a9a8ab2eec0e38f87a5bb419c0d9a5e7fe062
Im HyungJin <vf0918@gmail.com>
2023-08-25 14:56:54 +0900

    폴더 변경

---
+++ Day1/actor004/Program.cs
@@ -0,0 +1,43 @@
+﻿using Akka.Actor;
+using Akka.Configuration;
+using Actors;
+
+namespace Actor004
+{
+    class Program
+    {
+        /*
+            # ActorSelection    : 
+            # ActorSelection    : Identify, id
+            # Watch             : Terminated
+        */
+
+        static void Main(string[] args)
+        {
+            var lifecycleconfig = ConfigurationFactory.ParseString(@"
+                akka {
+                    actor.debug
+                    {
+                        lifecycle = on
+                    }
+                }");
+
+            var system = ActorSystem.Create("MyActorSystem004");//, lifecycleconfig);
+
+            var parent_king = system.ActorOf(KingParent.Props(), "King");
+            var parent_queen = system.ActorOf(QueenParent.Props(), "Queen");
+            var parent_jack = system.ActorOf(JackParent.Props(), "Jack");
+
+            //var selection = system.ActorSelection("/user/*");
+            //var selection = system.ActorSelection("/user/King");
+            //var selection = system.ActorSelection("/user/King/*");
+            //selection.Tell("selected!");
+
+            var killer = system.ActorOf(ActorFinder.Props(), "Killer");
+            //killer.Tell("/user/King");
+            killer.Tell("/user/King/*");
+
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ Day1/actor006/Program.cs
@@ -0,0 +1,22 @@
+﻿using Akka.Actor;
+using Actors;
+
+namespace Actor006
+{
+    class Program
+    {
+        /*
+            # SetReceiveTimeout : ReceiveTimeout Message, Interval
+        */
+
+        static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("MyActorSystem006");
+
+            var actor = system.ActorOf(TimeoutActor.Props(), "TimeoutActor");
+            actor.Tell("Hello");
+
+            Thread.Sleep(5000);
+        }
+    }
+}
